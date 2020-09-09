6511f76b513ddd0572c06e9b87f5d900790eca63
康超 <15934862314@163.com>
2020-09-09 10:39:29 +0800

    修改index.php文件

--- index.php
+++ index.php
@@ -5,14 +5,7 @@
  * Date: 2018/9/20
  * Time: 15:30
  */
-include_once "deviceInterface.php";
-$interfaceModel = new deviceInterface();
 $data = $_GET['data'];
 $datas = explode(',',$data);
 file_put_contents($datas[0] . ".txt", $data);
-if (count($datas) == 17) {
-    $result = $interfaceModel->MPM6881($datas);
-} else {
-    $result = "data structure error";
-}
-echo $result;
+echo $data;
